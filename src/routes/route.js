971452324e720accf935370171f10448097c7c47
Ashishraut92 <ashishraut000@gmail.com>
2022-02-22 05:19:59 -0800

    lodash added

--- src/routes/route.js
+++ src/routes/route.js
@@ -15,8 +15,23 @@ router.get('/test-me', function (req, res) {
 });
 router.get('/hello', function (req, res) {
     let arr = ["jan", "feb", "march","april","may", "june", "july", "august", "sept", "october", "november", "december" ]
-    console.log(lodash.chunk(arr,4))
+    console.log(lodash.chunk(arr,4));
+
+    let arr2 =[1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+    console.log(lodash.takeRightWhile(arr2));
+
+    let first = [9, 7, 6, 5, 4, 8, 5,]
+    let second = [7, 5, 6, 4, 12, 13]
+    let third =[9, 8, 6, 5, 4, 12, 11]
+    let fourth = [10, 14, 9, 8, 7, 5, 6]
+    let fifth = [20, 11, 5, 7, 6, 32, 45]
+
+    console.log(lodash.union(first,second,third,fourth,fifth));
+
+    let pairs = [['horror', 'The shining'], ['drama', 'Titanic'], ['Thriller', 'Baaz']];
+    console.log(lodash.fromPairs(pairs));
     res.send('my first lodash')
+
 })
 module.exports = router;
 // adding this comment for no reason
