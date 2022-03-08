9eeda8d2374a9aae729ca73be435d9e66748a088
Ashishraut92 <ashishraut000@gmail.com>
2022-03-08 09:30:54 +0530

    partially done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,10 +1,10 @@
 const { count } = require("console")
-const BookModel= require("../models/bookModel")
+const productModel= require("../models/productModel")
 
-const createBook= async function (req, res) {
+const createProduct= async function (req, res) {
     let data= req.body
 
-    let savedData= await BookModel.create(data)
+    let savedData= await productModel.create(data)
     res.send({msg: savedData})
 }
 
@@ -53,7 +53,7 @@ const deleteBooks= async function (req, res) {
 
 
 
-module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
-module.exports.updateBooks= updateBooks
-module.exports.deleteBooks= deleteBooks
+module.exports.createProduct= createProduct
+// module.exports.getBooksData= getBooksData
+// module.exports.updateBooks= updateBooks
+// module.exports.deleteBooks= deleteBooks
